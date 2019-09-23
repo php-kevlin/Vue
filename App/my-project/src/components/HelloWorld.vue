80a88e5ed2e19cad698d60fcd97d5075e04a39da
php-kevlin <2426609750@qq.com>
2019-09-23 18:20:56 +0800

    提交整个项目

---
+++ App/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+      <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'hello',
+  data(){
+  return {
+    message:'欢迎来到菜鸟教程'
+  }
+  }
+
+}
+</script>
+
